f3ca046f7b199db2734eb362de6f7650061316f0
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2023-04-30 13:12:49 -0600

    carrucel diferentes generos

--- src/movies/pages/Home.tsx
+++ src/movies/pages/Home.tsx
@@ -10,13 +10,13 @@ export const Home = () => {
             <Carrucel titulo='Acción' slug='/discover/movie?with_genres=28&' />
             <Carrucel titulo='Aventura' slug='/discover/movie?with_genres=12&' />
             <Carrucel titulo='Drama' slug='/discover/movie?with_genres=18&' />
-            <Carrucel titulo='Lo más visto' slug='/discover/movie?with_genres=16&' />
-            <Carrucel titulo='Lo más visto' slug='/discover/movie?with_genres=16&' />
-            <Carrucel titulo='Lo más visto' slug='/discover/movie?with_genres=16&' />
-            <Carrucel titulo='Lo más visto' slug='/discover/movie?with_genres=16&' />
-            <Carrucel titulo='Lo más visto' slug='/discover/movie?with_genres=16&' />
-            <Carrucel titulo='Lo más visto' slug='/discover/movie?with_genres=16&' />
-            <Carrucel titulo='Lo más visto' slug='/discover/movie?with_genres=16&' />
+            <Carrucel titulo='Comedia' slug='/discover/movie?with_genres=16&' />
+            <Carrucel titulo='Documentales' slug='/discover/movie?with_genres=99&' />
+            <Carrucel titulo='Familia' slug='/discover/movie?with_genres=10751&' />
+            <Carrucel titulo='Fantasia' slug='/discover/movie?with_genres=14&' />
+            <Carrucel titulo='Terror' slug='/discover/movie?with_genres=27&' />
+            <Carrucel titulo='Misterio' slug='/discover/movie?with_genres=9648&' />
+            <Carrucel titulo='Romance' slug='/discover/movie?with_genres=10749&' />
         </MainLayout>
 
     )
